8fc79d769301f53018ff3282023d016e22327240
venom12138 <1390852413@qq.com>
2024-10-30 00:33:50 -0500

    compress videos

---
+++ compress_videos.sh
@@ -0,0 +1,20 @@
+sudo ./comp_vid -i static/old_videos/pushing_T/PR_Crown1.mp4 -o static/videos/pushing_T/PR_Crown1.mp4 -f 3
+sudo ./comp_vid -i static/old_videos/pushing_T/PR_Crown2.mp4 -o static/videos/pushing_T/PR_Crown2.mp4 -f 3
+sudo ./comp_vid -i static/old_videos/pushing_T/PR_Crown3.mp4 -o static/videos/pushing_T/PR_Crown3.mp4 -f 3
+sudo ./comp_vid -i static/old_videos/pushing_T/PR_Crown4.mp4 -o static/videos/pushing_T/PR_Crown4.mp4 -f 3
+
+sudo ./comp_vid -i static/old_videos/robustness/robustness1.mp4 -o static/videos/robustness/robustness1.mp4 -f 3
+sudo ./comp_vid -i static/old_videos/robustness/robustness2.mp4 -o static/videos/robustness/robustness2.mp4 -f 3
+sudo ./comp_vid -i static/old_videos/robustness/robustness3.mp4 -o static/videos/robustness/robustness3.mp4 -f 3
+sudo ./comp_vid -i static/old_videos/robustness/robustness4.mp4 -o static/videos/robustness/robustness4.mp4 -f 3
+
+sudo ./comp_vid -i static/old_videos/rope_routing/Crown1.mp4 -o static/videos/rope_routing/Crown1.mp4 -f 3
+sudo ./comp_vid -i static/old_videos/rope_routing/Crown2.mp4 -o static/videos/rope_routing/Crown2.mp4 -f 3
+
+# sudo ./comp_vid -i static/videos/merging_L/PR_Crown1.mp4 -o static/videos/merging_L/PR_Crown1.mp4 -f 3
+# sudo ./comp_vid -i static/videos/merging_L/PR_Crown2.mp4 -o static/videos/merging_L/PR_Crown2.mp4 -f 3
+# sudo ./comp_vid -i static/videos/merging_L/PR_Crown3.mp4 -o static/videos/merging_L/PR_Crown3.mp4 -f 3
+
+# sudo ./comp_vid -i static/videos/merging_L/PR_Crown1.mp4 -o static/videos/merging_L/PR_Crown1.mp4 -f 3
+# sudo ./comp_vid -i static/videos/merging_L/PR_Crown2.mp4 -o static/videos/merging_L/PR_Crown2.mp4 -f 3
+# sudo ./comp_vid -i static/videos/merging_L/PR_Crown3.mp4 -o static/videos/merging_L/PR_Crown3.mp4 -f 3
